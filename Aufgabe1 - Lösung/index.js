bee3dbce799d23a9e15b23fb41284c8b46e31a3a
Methusshan Elankumaran <56478334+Methusshan25@users.noreply.github.com>
2024-06-08 17:00:45 +0200

    add Task 1

---
+++ Aufgabe1 - Lösung/index.js
@@ -0,0 +1,107 @@
+import * as THREE from "../node_modules/three/build/three.module.js";
+
+const canvas = document.getElementById('canvas');
+const gl = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true
+})
+
+gl.setSize(window.innerWidth - 20, window.innerHeight - 20)
+//setup camera
+const angleOfView = 55;
+const aspectRatio = canvas.clientWidth / canvas.clientHeight;
+const nearPlane = 0.1;
+const farPlane = 100;
+const camera = new THREE.PerspectiveCamera(
+    angleOfView,
+    aspectRatio,
+    nearPlane,
+    farPlane
+);
+camera.position.set(0, 8, 30);
+//making a scene
+const scene = new THREE.Scene()
+
+scene.background = new THREE.Color(0.01, 0.01, 0.01);
+
+
+const axesHelper = new THREE.AxesHelper(20);
+scene.add(axesHelper);
+
+
+//SPHERE
+
+const sphereRadius = 4;
+const sphereWidthSegments = 32;
+const sphereHeightSegments = 16;
+const sphereGeometry = new THREE.SphereGeometry(
+    sphereRadius,
+    sphereWidthSegments,
+    sphereHeightSegments
+);
+
+const textureLoader = new THREE.TextureLoader();
+
+
+const sphereColorMap = textureLoader.load('textures/brickBaseMap.jpg');
+sphereColorMap.wrapS = THREE.RepeatWrapping;
+sphereColorMap.wrapT = THREE.RepeatWrapping;
+sphereColorMap.repeat.set(4, 4)
+
+const sphereNormalMap = textureLoader.load('textures/brickNormalMap.jpg');
+sphereNormalMap.wrapS = THREE.RepeatWrapping;
+sphereNormalMap.wrapT = THREE.RepeatWrapping;
+sphereNormalMap.repeat.set(4, 4)
+
+const displacementMap = textureLoader.load('textures/brickDisplacementMap.png');
+displacementMap.wrapS = THREE.RepeatWrapping;
+displacementMap.wrapT = THREE.RepeatWrapping;
+displacementMap.repeat.set(4, 4)
+
+
+const sphereMaterial = new THREE.MeshStandardMaterial({
+    map: sphereColorMap,
+    normalMap: sphereNormalMap,
+    displacementMap: displacementMap
+})
+
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.set(0, 0, 0);
+scene.add(sphere);
+
+//LIGHT
+
+const color = 0xffffff;
+const intensity = 1;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(0, 30, 30);
+scene.add(light);
+
+//AMBIENT LIGHT
+const ambientColor = 0xffffff;
+const ambientIntensity = 0.2;
+const ambientLight = new THREE.AmbientLight(ambientColor, ambientIntensity);
+scene.add(ambientLight);
+
+
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    gl.setSize(window.innerWidth - 20, window.innerHeight - 20)
+    render()
+}
+
+function animate(time) {
+    time *= 0.001
+    requestAnimationFrame(animate)
+    sphere.rotation.x += 0.005;
+    render()
+}
+
+animate()
+
+function render() {
+    gl.render(scene, camera)
+}
---
+++ Aufgabe1/Aufgabenstellung.md
@@ -0,0 +1,31 @@
+# Aufgabe 1
+
+Besitzer/-in: Methusshan Elankumaran
+
+# Aufgabenstellung:
+
+**Erstelle eine Szene in three.js, die einen Würfel beinhaltet, die drei sich im Textur-Ordner befindenden Texturen enthält. Lasse diesen sich kontinuierlich um die X- und Y-Achse drehen. Füge ein Directional Light hinzu, welches auf die Kugel zeigt. Wrapping-Einstellungen für die Texturen können selbst gewählt werden.**
+
+# Einstellungen:
+
+Kamera hat folgende Einstellungen:
+
+- FieldOfView: 55
+- Aspect Ratio: canvas.clientWidth / canvas.clientHeight
+- Near Plane: 0.1
+- Far Plane: 100
+- Position: (0,8,30)
+
+Sphäre hat folgende Einstellungen:
+
+- Radius : 4
+- Width Segments : 32
+- Height Segments : 16
+
+Directional Light hat folgende Einstellungen:
+
+- Farbe: 0xffecb8
+- Intensität: 0.8
+- Position: (0,20,20)
+
+![alt text](result.png)
